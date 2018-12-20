c1529f99b4a69773e67fc7c0db36093264ef2009
OpenJ92 <jacob.vartuli.92@gmail.com>
2018-12-20 09:50:29 -0500

    Update to Mandelbrot file. Look to implement Mandlebrot plotter for next commit

--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -1,17 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-L = lambda x: x**2
+# incomplete:  Look to reform this itterated function so that we can generate the
+#   Mandelbrot Set
+
+L = lambda x, c: x*x
 L_con = lambda x: True if np.absolute(x) < 2 else False
 
 def r_Func(val, func = L, con_func = L_con, count = 0):
     func_val = func(val)
-    import pdb; pdb.set_trace()
-    if (con_func(func_val)) and (count <= 100):
+    #import pdb; pdb.set_trace()
+    if (con_func(func_val)) and (count < 100):
         count += 1
         print(count, func_val, np.absolute(func_val))
         return r_Func(func_val, func, con_func, count)
     else:
         return count
 
-sample = np.random.random_sample(size = (1000,)) + 1j * np.random.random_sample(size = (1000,))
-_resolved_samples = np.apply_along_axis(r_Func, 0, sample)
+sample = (np.random.random_sample(size = (10000,))) + (1j * np.random.random_sample(size = (10000,)))
+
+A = [r_Func(i) for i in sample]
+for i in range(0, len(sample)):
+     print(i, A[i], sample[i])
+     if A[i] > 50:
+         plt.scatter(sample[i].real, sample[i].imag)
+plt.show()
