8e6e4e40d0a00356d4b9d675d8b4944030bdd01b
OpenJ92 <jacob.vartuli.92@gmail.com>
2018-12-20 17:37:16 -0500

    generalization of r_Func constructed

--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -1,27 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# incomplete:  Look to reform this itterated function so that we can generate the
-#   Mandelbrot Set
+# incomplete:  how to I introduce an arbitrary amount of parameters to this function
+#   Note that function L has three parameters and by extension, r_func has 3 initial
+#   parameters. Is there a way to specify n parameters in one argument?
 
-L = lambda x, c: x*x
+L = lambda x, c, pow: (x**pow) + c
 L_con = lambda x: True if np.absolute(x) < 2 else False
 
-def r_Func(val, func = L, con_func = L_con, count = 0):
-    func_val = func(val)
+def r_Func(x, c, pow, func = L, con_func = L_con, count = 0):
     #import pdb; pdb.set_trace()
-    if (con_func(func_val)) and (count < 100):
+    func_val = func(x, c, pow)
+    if (con_func(func_val)) and (count < 500):
         count += 1
-        print(count, func_val, np.absolute(func_val))
-        return r_Func(func_val, func, con_func, count)
+        return r_Func(func_val, c, pow, func, con_func, count)
     else:
         return count
 
-sample = (np.random.random_sample(size = (10000,))) + (1j * np.random.random_sample(size = (10000,)))
+# generalized to arbitrary amount of parameters
 
-A = [r_Func(i) for i in sample]
-for i in range(0, len(sample)):
-     print(i, A[i], sample[i])
-     if A[i] > 50:
-         plt.scatter(sample[i].real, sample[i].imag)
-plt.show()
+def r_func(_, _con, _param, _con_param, count):
+    func_val = _(*_param)
+    if (_con(func_val, *_con_param)) and (count < 500):
+        count += 1
+        _param[0] = func_val
+        return r_func( _, _con, _param, _con_param, count)
+    else:
+        return count
+
+
+sample = ((4*np.random.random_sample(size = (100000,))) - 2) + ((4 * 1j * np.random.random_sample(size = (100000,))) - 2j)
+
+# for j in np.linspace(0, 4, 400):
+#     for i in sample:
+#         print(r_func(L, L_con, [0,i,j], [], 0), i, j)
+
+
+# for j in range(6, 400):
+#     A = np.linspace(.05, 4, 200)
+#     B = [r_Func(0, k, A[j]) for k in sample]
+#     for i in range(0, len(sample)):
+#          #print(i, B[i], sample[i])
+#          if B[i] > 10:
+#              if B[i] < 25:
+#                  plt.scatter(sample[i].real, sample[i].imag, s = 1, c = 'blue')
+#              elif B[i] < 100:
+#                  plt.scatter(sample[i].real, sample[i].imag, s = 1, c = 'red')
+#              else:
+#                  plt.scatter(sample[i].real, sample[i].imag, s = 1, c = 'green')
+#     print(j)
+#     plt.axis([-2, 2, -2, 2])
+#     plt.savefig(str(j) + '.jpeg')
+#     plt.close()
