15917d529ba667183e3382a061809a070a210274
OpenJ92 <jacob.vartuli.92@gmail.com>
2019-01-23 10:26:29 -0500

    continued work on generalized recursion function

--- MandelbrotSet.py
+++ MandelbrotSet.py
@@ -6,32 +6,35 @@ import plotly.graph_objs as go
 L = lambda x, c, pow: (x**pow) + c
 L_con = lambda x, count: True if (np.absolute(x)) < 2 and (count < 500) else False
 
+m = lambda x, mu, sigma: np.e**(-1*(x - mu)**2 / 2*np.sqrt(sigma))
+m_con = lambda x, count: True if count <= 500 else False
 
 # r_func usage:
 # r_func takes in a defined function (_) and a condition function (_con). Notice that
 # in this case, _(*_param) must evaluate to an element resolvable by _. In other words,
-# it must be _(*_param) =~ *_param. Secondly, 
+# it must be _(*_param) =~ *_param. Secondly,
 def r_func(_, _con, _param, _con_param):
     func_val = _(*_param)
     if _con(func_val, *_con_param):
+        print(_, _con, _param, _con_param)
         _con_param[-1] += 1
         _param[0] = func_val
         return r_func(_, _con, _param, _con_param)
     else:
-        return _con_param[-1]
+        return (_, _con, _param, _con_param)
 
 def plot_complex(complex):
     return plt.scatter(complex.real, complex.imag, c = 'blue')
 
 sample = ((4*np.random.random_sample(size = (75000,))) - 2) + ((4 * 1j * np.random.random_sample(size = (75000,))) - 2j)
 
-r = []
-for j in np.linspace(0, 4, 400):
-    q = []
-    for i in sample:
-        B = r_func(L, L_con, [0,i,j], [0])
-        q.append(B)
-    r.append(q)
+# r = []
+# for j in np.linspace(0, 4, 400):
+#     q = []
+#     for i in sample:
+#         B = r_func(L, L_con, [0,i,j], [0])
+#         q.append(B)
+#     r.append(q)
 
 # A = np.array(r)
 
